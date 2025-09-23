e5ba83b3e31286f4547eae76a59a11d9cb981d32
haytham2597 <haytham2597@gmail.com>
2025-09-23 18:11:04 +0000

    refactor master predicates

--- master/src/sched/predicates.h
+++ master/src/sched/predicates.h
@@ -5,7 +5,124 @@
 #include "base_scheduler.h"
 #endif
 
-//TODO: Fix circular-reference
+int is_valid_sp(state_process from, state_process to){
+    if(from ==STATE_READY && to != STATE_READY)
+        return 1;
+    if(from == STATE_EXEC && (to == STATE_READY || to == STATE_EXIT))
+        return 1;
+        
+    return 0;
+}
+
+int is_queue_sp(state_process sp){
+    return sp == STATE_READY;
+}
+int is_list_sp(state_process sp){
+    return !is_queue_sp(sp);
+}
+
+t_list* get_list_by_sp(state_process sp){
+    if(sp == STATE_READY){
+        log_error(logger, "State Process inválido para retorno de t_list: %s", state_to_string(sp));
+        return NULL;
+    }
+    return (t_list*)dictionary_get(dict_state, state_to_string(sp));
+}
+t_queue* get_queue_by_sp(state_process sp){
+    if(sp != STATE_READY){
+        log_error(logger, "State Process inválido para retorno de t_queue: %s", state_to_string(sp));
+        return NULL;
+    }
+    return (t_queue*)dictionary_get(dict_state, state_to_string(sp));
+    // debido al FIFO
+}
+
+
+
+void add_query_on_state(query* q, state_process sp){
+    if(is_queue_sp(sp)){
+        queue_push(get_queue_by_sp(sp), q);
+        return;
+    }
+    list_add(get_list_by_sp(sp), q);
+}
+
+int count_by_sp(state_process sp){
+    //void* elem = dictionary_get(dict_state,sp);
+    if(is_queue_sp(sp))
+        return queue_size(get_queue_by_sp(sp));
+    return list_size(get_list_by_sp(sp));
+}
+
+worker* cast_worker(void* elem){
+    if(elem == NULL){
+        log_warning(logger, "elem is null in (%s:%d)", __func__, __LINE__);
+        return NULL;
+    }
+    return (worker*)elem;
+}
+
+query* cast_query(void* elem){
+    if(elem == NULL){
+        log_warning(logger, "elem is null in (%s:%d)", __func__, __LINE__);
+        return NULL;
+    }
+    return (query*)elem;
+}
+int cast_int(void* elem){
+    return (int)elem;
+    //TODO: check this
+    /*int res = 0;
+    memcpy(&res, elem, sizeof(int));
+    return res;*/
+}
+
+bool order_query_by(void* a, void* b){
+    return cast_query(a)->priority < cast_query(b)->priority;
+}
+
+int by_worker_free(void* elem, void *by){
+    return ((worker*)elem)->is_free ==cast_int(by);
+}
+
+
+int by_worker_fd(void* elem, void *by){
+    return ((worker*)elem)->fd == cast_int(by);
+}
+
+int by_worker_wid(void* elem, void *by){
+    return ((worker*)elem)->id == cast_int(by);
+}
+
+int by_worker_qid(void* elem, void *by){
+    return ((worker*)elem)->id_query == cast_int(by);
+}
+
+int by_query_qid(void* elem, void* by){
+    return ((query*)elem)->id == cast_int(by);
+}
+
+worker* get_first_worker_free()
+{
+    void* w = list_find_by(workers, by_worker_free, (int)1);
+    return cast_worker(w);
+}
+
+worker* get_worker_by_fd(int fd, int* idx){
+    return cast_worker(list_find_by_idx_list(workers, by_worker_fd, fd, idx));
+}
+
+worker* get_worker_by_qid(qid id){
+    return cast_worker(list_find_by(workers, by_worker_qid, (int)id));
+}
+
+worker* get_worker_by_wid(wid id){
+    return cast_worker(list_find_by(workers, by_worker_wid, (int)id));
+}
+
+query* get_query_by_qid(qid id){
+    return cast_query(list_find_by(queries, by_query_qid, (int)id));
+}
 
 /*int by_worker_free(void* elem, void *by){
     worker* wa = (worker*)elem;
