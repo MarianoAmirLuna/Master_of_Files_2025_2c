f032b7ba9038be1afe8908bc401000db62e1d9f5
lseijasferraudo <lseijasferraudo@frba.utn.edu.ar>
2025-10-10 23:36:59 +0000

    vuelven los comentarios

--- storage/src/communications_ops/delete_tag.h
+++ storage/src/communications_ops/delete_tag.h
@@ -6,6 +6,16 @@
 #endif
 
 void delete_tag_ops(char* file, char* tag, worker* w){
+    /*Esta operación eliminará el directorio correspondiente al File:Tag indicado. 
+    Al realizar esta operación, si el bloque físico al que apunta cada bloque lógico eliminado 
+    no es referenciado por ningún otro File:Tag, deberá ser marcado como libre en el bitmap.*/
+    // Para la implementación de esta parte se recomienda consultar la documentación de la syscall stat(2).
+
+    /* EXCEPCIONES A TENER EN CUENTA EN ESTE PROCEDIMIENTO
+        File_inexistente
+        Tag_inexistente
+    */
+
     log_error(logger, "%s NOT IMPLEMENTED (%s:%d)",__func__, __func__,__LINE__);
     //Si necesitan decirle algo al worker desde este método se crea el paquet y se envía en w->fd send_and_free()
     //Ejemplo: send_and_free_packet(p, w->fd);
--- storage/src/communications_ops/read_block.h
+++ storage/src/communications_ops/read_block.h
@@ -6,6 +6,13 @@
 #endif
 
 void read_block_ops(char* file, char* tag, int numero_bloque, worker* w){
+    /*Dado un File:Tag y número de bloque lógico, la operación de lectura obtendrá y devolverá el contenido del mismo.*/
+    
+    /* EXCEPCIONES A TENER EN CUENTA EN ESTE PROCEDIMIENTO
+        File_inexistente
+        Tag_inexistente
+        Lectura_o_escritura_fuera_de_limite
+    */
     log_error(logger, "%s NOT IMPLEMENTED (%s:%d)",__func__, __func__,__LINE__);
     //Si necesitan decirle algo al worker desde este método se crea el paquet y se envía en w->fd send_and_free()
     //Ejemplo: send_and_free_packet(p, w->fd);
