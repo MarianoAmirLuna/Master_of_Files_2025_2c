9beb4758548a90f3cc9daa4370af874f73af89ff
haytham2597 <haytham2597@gmail.com>
2025-10-14 19:24:11 +0000

    possible leak

--- storage/src/communications_ops/create_file.h
+++ storage/src/communications_ops/create_file.h
@@ -16,20 +16,23 @@ void create_file_ops(char* file, char* tag, worker* w){
     char* path = string_from_format("%s/files", cs.punto_montaje);
     log_info(logger, "Ejecutando la operacion CREATE_FILE");
     crear_directorio(file, path); // creo el archivo (no verifico si existe)
-
+    free(path); //El string_from_format va a crear nueva dirección del puntero, por eso es aconsejable liberarlo antes de asignar otro
+    
     // creo el tag
     path = string_from_format("%s/%s", path, file); // uso el path porque es dentro del archivo
     crear_directorio(tag, path); // creo el tag
-
+    free(path); //El string_from_format va a crear nueva dirección del puntero, por eso es aconsejable liberarlo antes de asignar otro
+    
     // espacio de bloques logicos
     path = string_from_format("%s/%s", path, tag);
     crear_directorio("logical_blocks", path);
-
+    free(path); //El string_from_format va a crear nueva dirección del puntero, por eso es aconsejable liberarlo antes de asignar otro
+    
     // metadata
     path = string_from_format("%s/metadata.config", path);
-
     crear_metadata_config(path, g_block_size, NULL, WORK_IN_PROGRESS);
-    
+    free(path);
+
     log_orange(logger, "FILE: %s, TAG: %s", file, tag);
     if(w == NULL){
         log_error(logger, "EL WORKER ES NULO WTF");
@@ -38,8 +41,6 @@ void create_file_ops(char* file, char* tag, worker* w){
         log_debug(logger, "El qid es: %d", w->id_query);
     }
     log_info(logger, "## %d - File Creado %s:%s", w->id_query, file, tag);
-    
-    free(path);
     //Si necesitan decirle algo al worker desde este método se crea el paquet y se envía en w->fd send_and_free()
     //Ejemplo: send_and_free_packet(p, w->fd);
 }
