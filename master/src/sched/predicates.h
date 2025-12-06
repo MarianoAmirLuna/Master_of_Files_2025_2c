11bddb7ee3b01d4e73e5eda6f79b943a57cd9651
haytham2597 <haytham2597@gmail.com>
2025-12-06 19:17:47 +0000

    Pasando como log_debug un log molesto

--- master/src/sched/predicates.h
+++ master/src/sched/predicates.h
@@ -115,7 +115,7 @@ worker* get_first_worker_free()
         
     void* w = list_find(workers, by_worker_free);
     if(w == NULL){
-        log_warning(logger, "No hay workers libres");
+        log_debug(logger, "No hay workers libres");
         return NULL;
         //log_error(logger, "Ohhh es nulo el elemento wtf");
     }
