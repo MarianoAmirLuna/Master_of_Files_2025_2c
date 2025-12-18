a887f389612a6ba7560db476c0e406d3f5bf015e
haytham2597 <haytham2597@gmail.com>
2025-12-18 18:19:36 +0000

    planif

--- errores_query.sh
+++ errores_query.sh
@@ -2,6 +2,7 @@ run_query()
 {
 	echo "Run Script $1"
 	query_control/bin/query_control "query_control/storage.config" "$1" $2 &
+	sleep 0.25
 }
 run_query ESCRITURA_ARCHIVO_COMMITED 1
 run_query FILE_EXISTENTE 1
